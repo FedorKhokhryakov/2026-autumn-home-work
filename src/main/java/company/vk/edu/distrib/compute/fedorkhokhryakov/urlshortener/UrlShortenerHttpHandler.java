6d96d97b8277720275490680c844105e63b02a67
FedorKhokhryakov <fekhokh@gmail.com>
2026-09-25 11:00:47 +0300

    fix Codacy Static Code Analysis

--- src/main/java/company/vk/edu/distrib/compute/fedorkhokhryakov/urlshortener/UrlShortenerHttpHandler.java
+++ src/main/java/company/vk/edu/distrib/compute/fedorkhokhryakov/urlshortener/UrlShortenerHttpHandler.java
@@ -7,211 +7,231 @@ import java.nio.charset.StandardCharsets;
 import java.util.NoSuchElementException;
 
 public class UrlShortenerHttpHandler {
-    private static final String CONTENT_TYPE = "text/html; charset=utf-8";
+private static final String CONTENT_TYPE = "text/html; charset=utf-8";
+private static final String GET = "GET";
+private static final String POST = "POST";
+private static final String PUT = "PUT";
+private static final String DELETE = "DELETE";
+private static final String LINKS_PATH = "/v0/links";
+private static final String ROOT_PATH = "/";
+private static final String LOCATION = "Location";
+private static final String WWW_AUTHENTICATE = "WWW-Authenticate";
+private static final String BASIC = "Basic";
 
-    private final int port;
-    private final InMemoryDao<String> dao;
-    private final InMemoryUserDao userDao;
-    private final BasicAuthenticator authenticator;
+private final int port;
+private final InMemoryDao<String> dao;
+private final InMemoryUserDao userDao;
+private final BasicAuthenticator authenticator;
 
-    public UrlShortenerHttpHandler(
-        int port,
-        InMemoryDao<String> dao,
-        InMemoryUserDao userDao
-    ) {
-        this.port = port;
-        this.dao = dao;
-        this.userDao = userDao;
-        this.authenticator = new BasicAuthenticator(userDao);
+public UrlShortenerHttpHandler(
+    int port,
+    InMemoryDao<String> dao,
+    InMemoryUserDao userDao
+) {
+    this.port = port;
+    this.dao = dao;
+    this.userDao = userDao;
+    this.authenticator = new BasicAuthenticator(userDao);
+}
+
+public void handleStatus(HttpExchange exchange) throws IOException {
+    if (!GET.equals(exchange.getRequestMethod())) {
+        sendResponse(exchange, 404, "");
+        return;
     }
 
-    public void handleStatus(HttpExchange exchange) throws IOException {
-        if (!"GET".equals(exchange.getRequestMethod())) {
-            sendResponse(exchange, 404, "");
-            return;
-        }
+    sendResponse(exchange, 200, "");
+}
 
-        sendResponse(exchange, 200, "");
+public void handleLinks(HttpExchange exchange) throws IOException {
+    if (!authenticator.authenticate(exchange)) {
+        sendUnauthorized(exchange);
+        return;
     }
 
-    public void handleLinks(HttpExchange exchange) throws IOException {
-        if (!authenticator.authenticate(exchange)) {
-            sendUnauthorized(exchange);
-            return;
-        }
+    String path = exchange.getRequestURI().getPath();
 
-        String path = exchange.getRequestURI().getPath();
-        String prefix = "/v0/links";
-
-        if ("/v0/links".equals(path) || "/v0/links/".equals(path)) {
-            if ("POST".equals(exchange.getRequestMethod())) {
-                handleCreate(exchange);
-            } else {
-                sendResponse(exchange, 404, "");
-            }
-            return;
-        }
-
-        if (!path.startsWith(prefix + "/")) {
-            sendResponse(exchange, 404, "");
-            return;
-        }
-
-        String id = path.substring((prefix + "/").length());
-
-        if (!UrlShortenerUtils.isValidId(id)) {
-            sendResponse(exchange, 422, "");
-            return;
-        }
-
-        switch (exchange.getRequestMethod()) {
-            case "GET" -> handleGet(exchange, id);
-            case "PUT" -> handleUpdate(exchange, id);
-            case "DELETE" -> handleDelete(exchange, id);
-            default -> sendResponse(exchange, 404, "");
-        }
+    if (isLinksRoot(path)) {
+        handleLinksRoot(exchange);
+        return;
     }
 
-    public void handleRedirect(HttpExchange exchange) throws IOException {
-        if (!"GET".equals(exchange.getRequestMethod())) {
-            sendResponse(exchange, 404, "");
-            return;
-        }
-
-        String path = exchange.getRequestURI().getPath();
-
-        if (path.length() <= 1 || !path.startsWith("/")) {
-            sendResponse(exchange, 404, "");
-            return;
-        }
-
-        String id = path.substring(1);
-
-        if (!UrlShortenerUtils.isValidId(id)) {
-            sendResponse(exchange, 422, "");
-            return;
-        }
-
-        try (exchange) {
-            try {
-                String longLink = dao.get(id);
-
-                exchange.getResponseHeaders().set("Location", longLink);
-                exchange.sendResponseHeaders(301, -1);
-            } catch (NoSuchElementException e) {
-                sendResponse(exchange, 404, "");
-            }
-        }
+    if (!path.startsWith(LINKS_PATH + ROOT_PATH)) {
+        sendResponse(exchange, 404, "");
+        return;
     }
 
-    public void handleUsers(HttpExchange exchange) throws IOException {
-        if (!"POST".equals(exchange.getRequestMethod())) {
-            sendResponse(exchange, 404, "");
-            return;
-        }
+    String id = path.substring((LINKS_PATH + ROOT_PATH).length());
 
-        String body = readBody(exchange);
-        int separator = body.indexOf(':');
-
-        if (separator <= 0 || separator == body.length() - 1) {
-            sendResponse(exchange, 422, "");
-            return;
-        }
-
-        String username = body.substring(0, separator);
-        String password = body.substring(separator + 1);
-
-        userDao.upsert(username, password);
-        sendResponse(exchange, 200, "");
+    if (!UrlShortenerUtils.isValidId(id)) {
+        sendResponse(exchange, 422, "");
+        return;
     }
 
-    private void handleCreate(HttpExchange exchange) throws IOException {
-        String longLink = readBody(exchange);
+    handleLinkById(exchange, id);
+}
 
-        if (!UrlShortenerUtils.isValidLink(longLink)) {
-            sendResponse(exchange, 422, "");
-            return;
-        }
-
-        String id;
-
-        do {
-            id = UrlShortenerUtils.generateId();
-        } while (exists(id));
-
-        dao.upsert(id, longLink);
-
-        String shortLink = "http://localhost:" + port + "/" + id;
-
-        sendResponse(exchange, 201, shortLink);
+public void handleRedirect(HttpExchange exchange) throws IOException {
+    if (!GET.equals(exchange.getRequestMethod())) {
+        sendResponse(exchange, 404, "");
+        return;
     }
 
-    private void handleGet(HttpExchange exchange, String id) throws IOException {
+    String path = exchange.getRequestURI().getPath();
+
+    if (path.length() <= 1 || !path.startsWith(ROOT_PATH)) {
+        sendResponse(exchange, 404, "");
+        return;
+    }
+
+    String id = path.substring(ROOT_PATH.length());
+
+    if (!UrlShortenerUtils.isValidId(id)) {
+        sendResponse(exchange, 422, "");
+        return;
+    }
+
+    try (exchange) {
         try {
             String longLink = dao.get(id);
-            sendResponse(exchange, 200, longLink);
+
+            exchange.getResponseHeaders().set(LOCATION, longLink);
+            exchange.sendResponseHeaders(301, -1);
         } catch (NoSuchElementException e) {
             sendResponse(exchange, 404, "");
         }
     }
+}
 
-    private void handleUpdate(HttpExchange exchange, String id) throws IOException {
-        String longLink = readBody(exchange);
-
-        if (!UrlShortenerUtils.isValidLink(longLink)) {
-            sendResponse(exchange, 422, "");
-            return;
-        }
-
-        try {
-            dao.get(id);
-        } catch (NoSuchElementException e) {
-            sendResponse(exchange, 404, "");
-            return;
-        }
-
-        dao.upsert(id, longLink);
-        sendResponse(exchange, 200, "");
+public void handleUsers(HttpExchange exchange) throws IOException {
+    if (!POST.equals(exchange.getRequestMethod())) {
+        sendResponse(exchange, 404, "");
+        return;
     }
 
-    private void handleDelete(HttpExchange exchange, String id) throws IOException {
-        dao.delete(id);
-        sendResponse(exchange, 202, "");
+    String body = readBody(exchange);
+    int separator = body.indexOf(':');
+
+    if (separator <= 0 || separator == body.length() - 1) {
+        sendResponse(exchange, 422, "");
+        return;
     }
 
-    private String readBody(HttpExchange exchange) throws IOException {
-        return new String(
-            exchange.getRequestBody().readAllBytes(),
-            StandardCharsets.UTF_8
-        );
+    String username = body.substring(0, separator);
+    String password = body.substring(separator + 1);
+
+    userDao.upsert(username, password);
+    sendResponse(exchange, 200, "");
+}
+
+private void handleLinksRoot(HttpExchange exchange) throws IOException {
+    if (POST.equals(exchange.getRequestMethod())) {
+        handleCreate(exchange);
+    } else {
+        sendResponse(exchange, 404, "");
+    }
+}
+
+private void handleLinkById(HttpExchange exchange, String id) throws IOException {
+    switch (exchange.getRequestMethod()) {
+        case GET -> handleGet(exchange, id);
+        case PUT -> handleUpdate(exchange, id);
+        case DELETE -> handleDelete(exchange, id);
+        default -> sendResponse(exchange, 404, "");
+    }
+}
+
+private void handleCreate(HttpExchange exchange) throws IOException {
+    String longLink = readBody(exchange);
+
+    if (!UrlShortenerUtils.isValidLink(longLink)) {
+        sendResponse(exchange, 422, "");
+        return;
     }
 
-    private boolean exists(String id) {
-        try {
-            dao.get(id);
-            return true;
-        } catch (NoSuchElementException e) {
-            return false;
-        } catch (IOException e) {
-            throw new IllegalStateException("Failed to access DAO", e);
-        }
+    String id;
+
+    do {
+        id = UrlShortenerUtils.generateId();
+    } while (exists(id));
+
+    dao.upsert(id, longLink);
+
+    String shortLink = "http://localhost:" + port + "/" + id;
+
+    sendResponse(exchange, 201, shortLink);
+}
+
+private void handleGet(HttpExchange exchange, String id) throws IOException {
+    try {
+        String longLink = dao.get(id);
+        sendResponse(exchange, 200, longLink);
+    } catch (NoSuchElementException e) {
+        sendResponse(exchange, 404, "");
+    }
+}
+
+private void handleUpdate(HttpExchange exchange, String id) throws IOException {
+    String longLink = readBody(exchange);
+
+    if (!UrlShortenerUtils.isValidLink(longLink)) {
+        sendResponse(exchange, 422, "");
+        return;
     }
 
-    private static void sendResponse(HttpExchange exchange, int status, String body)
-        throws IOException {
-        byte[] bytes = body.getBytes(StandardCharsets.UTF_8);
-
-        exchange.getResponseHeaders().set("Content-Type", CONTENT_TYPE);
-        exchange.sendResponseHeaders(status, bytes.length);
-
-        try (var responseBody = exchange.getResponseBody()) {
-            responseBody.write(bytes);
-        }
+    try {
+        dao.get(id);
+    } catch (NoSuchElementException e) {
+        sendResponse(exchange, 404, "");
+        return;
     }
 
-    private void sendUnauthorized(HttpExchange exchange) throws IOException {
-        exchange.getResponseHeaders().set("WWW-Authenticate", "Basic");
-        sendResponse(exchange, 401, "");
+    dao.upsert(id, longLink);
+    sendResponse(exchange, 200, "");
+}
+
+private void handleDelete(HttpExchange exchange, String id) throws IOException {
+    dao.delete(id);
+    sendResponse(exchange, 202, "");
+}
+
+private String readBody(HttpExchange exchange) throws IOException {
+    return new String(
+        exchange.getRequestBody().readAllBytes(),
+        StandardCharsets.UTF_8
+    );
+}
+
+private boolean exists(String id) {
+    try {
+        dao.get(id);
+        return true;
+    } catch (NoSuchElementException e) {
+        return false;
+    } catch (IOException e) {
+        throw new IllegalStateException("Failed to access DAO", e);
     }
+}
+
+private static void sendResponse(HttpExchange exchange, int status, String body)
+    throws IOException {
+    byte[] bytes = body.getBytes(StandardCharsets.UTF_8);
+
+    exchange.getResponseHeaders().set("Content-Type", CONTENT_TYPE);
+    exchange.sendResponseHeaders(status, bytes.length);
+
+    try (var responseBody = exchange.getResponseBody()) {
+        responseBody.write(bytes);
+    }
+}
+
+private void sendUnauthorized(HttpExchange exchange) throws IOException {
+    exchange.getResponseHeaders().set(WWW_AUTHENTICATE, BASIC);
+    sendResponse(exchange, 401, "");
+}
+
+private static boolean isLinksRoot(String path) {
+    return LINKS_PATH.equals(path) || (LINKS_PATH + ROOT_PATH).equals(path);
+}
 
 }
--- src/main/java/company/vk/edu/distrib/compute/fedorkhokhryakov/urlshortener/UrlShortenerServiceImpl.java
+++ src/main/java/company/vk/edu/distrib/compute/fedorkhokhryakov/urlshortener/UrlShortenerServiceImpl.java
@@ -5,57 +5,61 @@ import com.sun.net.httpserver.HttpServer;
 
 import java.io.IOException;
 import java.net.InetSocketAddress;
+import java.util.Optional;
 
 public class UrlShortenerServiceImpl implements UrlShortenerService {
-    private final int port;
-    private final InMemoryDao<String> dao;
-    private final InMemoryUserDao userDao;
-    private HttpServer server;
+private final int port;
+private final InMemoryDao<String> dao;
+private final InMemoryUserDao userDao;
+private Optional<HttpServer> server = Optional.empty();
 
-    public UrlShortenerServiceImpl(int port, InMemoryDao<String> dao) {
-        this(port, dao, new InMemoryUserDao());
+public UrlShortenerServiceImpl(int port, InMemoryDao<String> dao) {
+    this(port, dao, new InMemoryUserDao());
+}
+
+public UrlShortenerServiceImpl(
+    int port,
+    InMemoryDao<String> dao,
+    InMemoryUserDao userDao
+) {
+    this.port = port;
+    this.dao = dao;
+    this.userDao = userDao;
+}
+
+@Override
+public void start() {
+    if (server.isPresent()) {
+        throw new IllegalStateException("Service has already been started");
     }
 
-    public UrlShortenerServiceImpl(
-        int port,
-        InMemoryDao<String> dao,
-        InMemoryUserDao userDao
-    ) {
-        this.port = port;
-        this.dao = dao;
-        this.userDao = userDao;
+    try {
+        HttpServer httpServer =
+            HttpServer.create(new InetSocketAddress("localhost", port), 0);
+
+        UrlShortenerHttpHandler handler =
+            new UrlShortenerHttpHandler(port, dao, userDao);
+
+        httpServer.createContext("/v0/status", handler::handleStatus);
+        httpServer.createContext("/v0/links", handler::handleLinks);
+        httpServer.createContext("/internal/users", handler::handleUsers);
+        httpServer.createContext("/", handler::handleRedirect);
+
+        httpServer.start();
+        server = Optional.of(httpServer);
+    } catch (IOException e) {
+        throw new IllegalStateException("Failed to start HTTP server", e);
     }
+}
 
-    @Override
-    public void start() {
-        if (server != null) {
-            throw new IllegalStateException("Service has already been started");
-        }
+@Override
+public void stop() {
+    HttpServer httpServer = server.orElseThrow(
+        () -> new IllegalStateException("Service has not been started")
+    );
 
-        try {
-            server = HttpServer.create(new InetSocketAddress("localhost", port), 0);
+    httpServer.stop(0);
+    server = Optional.empty();
+}
 
-            UrlShortenerHttpHandler handler =
-                new UrlShortenerHttpHandler(port, dao, userDao);
-
-            server.createContext("/v0/status", handler::handleStatus);
-            server.createContext("/v0/links", handler::handleLinks);
-            server.createContext("/internal/users", handler::handleUsers);
-            server.createContext("/", handler::handleRedirect);
-
-            server.start();
-        } catch (IOException e) {
-            throw new IllegalStateException("Failed to start HTTP server", e);
-        }
-    }
-
-    @Override
-    public void stop() {
-        if (server == null) {
-            throw new IllegalStateException("Service has not been started");
-        }
-
-        server.stop(0);
-        server = null;
-    }
 }
